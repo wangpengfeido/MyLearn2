c4a1dc00a237bb4fda6172316e07312265f2c1fd
Wangpengfei <1036793130@qq.com>
2018-10-03 22:17:25 +0800

    ...

--- LearnReact/010default/my-app/src/app/aba-advanced-guides/aba-advanced-guides.js
+++ LearnReact/010default/my-app/src/app/aba-advanced-guides/aba-advanced-guides.js
@@ -12,6 +12,9 @@ import {AnaReconciliation} from "./ana-reconciliation/ana-reconciliation";
 import {AcaContext} from "./aca-context/aca-context";
 import {AgaHigherOrderComponents} from "./aga-higher-order-components/aga-higher-order-components";
 import {AeaForwardingRefs} from "./aea-forwarding-refs/aea-forwarding-refs";
+import {AfaFragments} from "./afa-fragments/afa-fragments";
+import {AkaProtals} from "./aka-portals/aka-portals";
+import {ApaRenderProps} from "./apa-render-props/apa-render-props";
 
 
 export class AbaAdvancedGuides extends Component {
@@ -22,10 +25,13 @@ export class AbaAdvancedGuides extends Component {
                 <Route path="/aba-advanced-guides/aba-code-splitting" component={AbaCodeSplitting}/>
                 <Route path="/aba-advanced-guides/aca-context" component={AcaContext}/>
                 <Route path="/aba-advanced-guides/aea-forwarding-refs" component={AeaForwardingRefs}/>
+                <Route path="/aba-advanced-guides/afa-fragments" component={AfaFragments}/>
                 <Route path="/aba-advanced-guides/aga-higher-order-components" component={AgaHigherOrderComponents}/>
                 <Route path="/aba-advanced-guides/aia-jsx-in-depth" component={AiaJsxInDepth}/>
+                <Route path="/aba-advanced-guides/aka-protals" component={AkaProtals}/>
                 <Route path="/aba-advanced-guides/ana-reconciliation" component={AnaReconciliation}/>
                 <Route path="/aba-advanced-guides/aoa-refs-and-the-dom" component={AoaRefsAndTheDom}/>
+                <Route path="/aba-advanced-guides/apa-render-props" component={ApaRenderProps}/>
                 <Route path="/aba-advanced-guides/aqa-static-type-checking" component={AqaStaticTypeChecking}/>
                 <Route path="/aba-advanced-guides/asa-typechecking-with-proptypes" component={AsaTypecheckingWithProptypes}/>
                 <Route path="/aba-advanced-guides/ata-uncontrolled-component" component={AtaUnControlledComponent}/>
---
+++ LearnReact/010default/my-app/src/app/aba-advanced-guides/apa-render-props/apa-render-props.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export class ApaRenderProps extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+
